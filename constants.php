73ace86e82d9261727162f522055de60609c363e
Marco Kilchhofer <marco@kilchhofer.info>
2017-10-26 15:41:32 +0200

    Implement php nav

---
+++ constants.php
@@ -0,0 +1,33 @@
+<?php
+$sites = [
+    'dashboard' => [
+        'title' => 'Dashboard',
+        'pos' => 'left',
+        'icon' => 'glyphicon-dashboard'
+    ],
+    'my' => [
+        'title' => 'My Gear',
+        'pos' => 'left',
+        'icon' => 'glyphicon-list'
+    ],
+    'marketplace' => [
+        'title' => 'Dashboard',
+        'pos' => 'left',
+        'icon' => 'glyphicon-shopping-cart'
+    ],
+    'about' => [
+        'title' => 'About',
+        'pos' => 'right',
+        'icon' => 'glyphicon-info-sign'
+    ],
+    'login' => [
+        'title' => 'Login',
+        'pos' => 'right',
+        'icon' => 'glyphicon-user'
+    ],
+    'logout' => [
+        'title' => 'Logout',
+        'pos' => 'right',
+        'icon' => 'glyphicon-log-in'
+    ],
+];
---
+++ nav.php
@@ -0,0 +1,24 @@
+<?php
+require_once ('constants.php');
+
+echo "<ul class=\"nav navbar-nav\">";
+
+foreach ($sites as $key => $site) {
+    if ($site['pos'] == "left") {
+        if ($site['icon'] != null) {
+            $icon = "<span class=\"glyphicon " . $site['icon'] . "\"></span>";
+        }
+        echo "<li><a href=\"?s=" . $key . "\">" . $icon . $site['title'] . "</a></li>";
+    }
+}
+echo "</ul><ul class=\"nav navbar-nav navbar-right\">";
+
+foreach ($sites as $key => $site) {
+    if ($site['pos'] == "right") {
+        if ($site['icon'] != null) {
+            $icon = "<span class=\"glyphicon " . $site['icon'] . "\"></span>";
+        }
+        echo "<li><a href=\"?s=" . $key . "\">" . $icon . $site['title'] . "</a></li>";
+    }
+}
+echo "</ul>";

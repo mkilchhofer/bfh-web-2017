005dd265d3c1627d0e41dc57a9b5922ff7f55fdc
Marco Kilchhofer <marco@kilchhofer.info>
2017-10-27 13:40:24 +0200

    Set title via php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+<?php
+require_once ('constants.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>MyGear.</title>
+    <title>MyGear.<?php echo " - ".$sites[$_GET['s']]['title']; ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">

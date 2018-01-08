90efef676477dd0af1361081ab48cca9c9612f6e
Marco Kilchhofer <marco@kilchhofer.info>
2018-01-08 22:29:33 +0100

    Use image gallery from github.com/blueimp/Gallery

--- src/TemplateHelper.php
+++ src/TemplateHelper.php
@@ -15,8 +15,22 @@ class TemplateHelper {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <link href="/css/custom.css" rel="stylesheet">
+    <script src="https://use.fontawesome.com/4574efd0ac.js"></script>
+    <link rel="stylesheet" href="/modules/blueimp-gallery/css/blueimp-gallery.min.css">
 </head>
 <body>
+<script src="/modules/blueimp-gallery/js/blueimp-gallery.min.js"></script>
+
+<!-- The Gallery as lightbox dialog, should be a child element of the document body -->
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+    <div class="slides"></div>
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <a class="play-pause"></a>
+    <ol class="indicator"></ol>
+</div>
 HEAD;
 
         require_once('core/nav.php');
@@ -40,6 +54,16 @@ EOF;
         <span class="text-muted">MyGear. {$githubVersion} - Created with ♡ by {$contributors}</span>
       </div>
     </footer>
+    <script>
+    document.getElementById('links').onclick = function (event) {
+        event = event || window.event;
+        var target = event.target || event.srcElement,
+            link = target.src ? target.parentNode : target,
+            options = {index: link, event: event},
+            links = this.getElementsByTagName('a');
+        blueimp.Gallery(links, options);
+    };
+    </script>
 </body>
 </html>
 EOF;
